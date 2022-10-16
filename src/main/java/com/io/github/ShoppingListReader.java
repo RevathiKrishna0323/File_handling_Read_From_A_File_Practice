3d8b1ec5cf644e8a7a9498b5137075f691a7c57f
Revathi G <revathikrishna0323@gmail.com>
2022-10-16 22:03:00 +0530

    declared and initiaised file input stream object in the ShoppingListReader class, handled the exceptions that occured and printed the exception message

--- src/main/java/com/io/github/ShoppingListReader.java
+++ src/main/java/com/io/github/ShoppingListReader.java
@@ -6,10 +6,19 @@
 
 package com.io.github;
 
-public class ShoppingListReader {
-    private static final String FILE_PATH = "src/main/resources/data/ShoppingList.csv";
-    public void readShoppingList(){
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 
+public class ShoppingListReader {
+    private static final String FILE_NAME = "src/main/resources/data/ShoppingList.csv";
+    public void readShoppingList(){
+    // declare and initiaise file input stream object
+        try {
+            FileInputStream fileInputStream = new FileInputStream(FILE_NAME)
+        } catch (FileNotFoundException exception) {
+            throw new RuntimeException(exception);
+            System.err.println(exception.getMessage());
+        }
 
     }
 }
